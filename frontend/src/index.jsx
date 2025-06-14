a721756b1a52e29640ba2ac7936b0d7371c2bf2c
janogorkiewicz <163695092+janogorkiewicz@users.noreply.github.com>
2025-06-14 15:46:10 +0000

    Added <AuthContextProvider> wrapper to <App /> in index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-+     <AuthContextProvider>
+      <AuthContextProvider>
         <App />
-+     </AuthContextProvider>
+      </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
