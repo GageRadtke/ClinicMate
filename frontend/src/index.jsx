7fb37901ecc8c145613c6d3c8bef554dddee656d
janogorkiewicz <163695092+janogorkiewicz@users.noreply.github.com>
2025-06-14 15:31:22 +0000

    Added <AuthContextProvider> wrapping to <App /> in index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,14 +1,18 @@
+// src/index.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./tailwind.css";
+import AuthContextProvider from "./context/AuthContext";
 import App from "./App";
+import "./tailwind.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
++     <AuthContextProvider>
+        <App />
++     </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
